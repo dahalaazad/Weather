2486ef714d192c5b2509c0815f9d5fff01bb45f9
aazad.dahal <aazad.dahal@gurzu.net>
2022-12-15 13:30:22 +0545

    feat: Finish initial setup

---
+++ src/screens/Home/components/HourlyCard.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const HourlyCard = () => {
+  return (
+    <View>
+      <Text>HourlyCard</Text>
+    </View>
+  );
+};
+
+export default HourlyCard;
---
+++ src/screens/WeatherDetails.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const WeatherDetails = () => {
+  return (
+    <View>
+      <Text>WeatherDetails</Text>
+    </View>
+  );
+};
+
+export default WeatherDetails;
