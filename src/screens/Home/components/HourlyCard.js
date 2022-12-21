ef293f05bdc05371258834c61dd560bb261f726c
aazad.dahal <aazad.dahal@gurzu.net>
2022-12-21 23:43:30 +0545

    feat: Add svg icon to hourly card

--- src/screens/Home/components/HourlyCard.js
+++ src/screens/Home/components/HourlyCard.js
@@ -1,12 +1,32 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import React from 'react';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import LinearGradient from 'react-native-linear-gradient';
+import {Colors} from '@app/constants';
+import {WeatherIcon} from '@app/assets/svg';
 
 const HourlyCard = () => {
   return (
-    <View>
-      <Text>HourlyCard</Text>
-    </View>
+    <TouchableOpacity style={styles.container}>
+      <WeatherIcon />
+      <Text>Time</Text>
+    </TouchableOpacity>
   );
 };
 
 export default HourlyCard;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderRadius: hp('2%'),
+    justifyContent: 'center',
+    marginHorizontal: 5,
+    alignItems: 'center',
+    padding: wp('4%'),
+    backgroundColor: Colors.hourlyCardColor,
+  },
+});
