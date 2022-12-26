ec645919049d40d64edb565d43ff0658328bd8a0
aazad.dahal <aazad.dahal@gurzu.net>
2022-12-26 12:53:46 +0545

    fix: Remove underline from text input field

--- src/screens/SearchDetails/components/Search.js
+++ src/screens/SearchDetails/components/Search.js
@@ -1,11 +1,17 @@
 import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 import {TextInput} from 'react-native-paper';
+import {Colors} from '@app/constants';
 
 const Search = () => {
   return (
     <View style={styles.container}>
-      <TextInput label="Search your city" style={styles.textInputStyle} />
+      <TextInput
+        label="Search your city"
+        style={styles.textInputStyle}
+        selectionColor={Colors.blackColor}
+        activeUnderlineColor="transparent"
+      />
     </View>
   );
 };
