464f8aca9b3335d79bf9956bbf4ee39a2a852e02
aazad.dahal <aazad.dahal@gurzu.net>
2022-12-26 12:30:49 +0545

    fix: Center weather icon in hourly card

--- src/screens/Home/components/HourlyCard.js
+++ src/screens/Home/components/HourlyCard.js
@@ -10,7 +10,9 @@ import {WeatherIcon} from '@app/assets/svg';
 const HourlyCard = ({time}) => {
   return (
     <TouchableOpacity style={styles.container}>
-      <WeatherIcon />
+      <View style={{paddingLeft: hp('2%')}}>
+        <WeatherIcon />
+      </View>
 
       <Text style={styles.textStyle}>{time}</Text>
     </TouchableOpacity>
@@ -26,7 +28,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginHorizontal: 5,
     alignItems: 'center',
-    padding: wp('3%'),
+    height: hp('11%'),
+    width: wp('20%'),
     backgroundColor: Colors.hourlyCardColor,
   },
   textStyle: {
