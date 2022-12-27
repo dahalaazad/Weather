72189b3816fe8f54b7985f8754765ea44fae79b7
aazad.dahal <aazad.dahal@gurzu.net>
2022-12-27 23:30:31 +0545

    feat: Get weather icon from API

--- src/screens/Home/components/HourlyCard.js
+++ src/screens/Home/components/HourlyCard.js
@@ -1,17 +1,21 @@
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import React from 'react';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 import {Colors} from '@app/constants';
-import {WeatherIcon} from '@app/assets/svg';
 
-const HourlyCard = ({time}) => {
+const HourlyCard = ({icon, time}) => {
   return (
     <TouchableOpacity style={styles.container}>
-      <View style={{paddingLeft: hp('2%')}}>
-        <WeatherIcon />
+      <View style={{paddingBottom: hp('1%')}}>
+        <Image
+          style={styles.tinyLogo}
+          source={{
+            uri: `https://openweathermap.org/img/wn/${icon}@2x.png`,
+          }}
+        />
       </View>
 
       <Text style={styles.textStyle}>{time}</Text>
@@ -30,12 +34,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     height: hp('11%'),
     width: wp('19%'),
-    backgroundColor: Colors.hourlyCardColor,
+    backgroundColor: 'rgba(0,0,0,0.3)',
   },
   textStyle: {
     fontFamily: 'Poppins',
     fontWeight: '600',
     color: Colors.whiteColor,
     fontSize: 15,
+  },
+  tinyLogo: {
+    width: wp('15%'),
+    height: hp('5%'),
   },
 });
