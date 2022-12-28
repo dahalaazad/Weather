f0a8a4066e43ab26114d723896728046836e245b
aazad.dahal <aazad.dahal@gurzu.net>
2022-12-28 07:11:16 +0545

    feat: Pass city name while dispatching action

--- src/redux/slices/weatherData/weatherSlice.js
+++ src/redux/slices/weatherData/weatherSlice.js
@@ -3,34 +3,37 @@ import axios from 'axios';
 
 const initialState = {};
 
-export const getWeather = createAsyncThunk('weather/getWeather', async () => {
-  const baseURL = 'https://api.openweathermap.org/data/2.5';
-  const appId = 'b31f986179635820b5d31e28a4cf9fc2';
-  const cityName = 'Kathmandu';
+export const getWeather = createAsyncThunk(
+  'weather/getWeather',
+  async cityName => {
+    const baseURL = 'https://api.openweathermap.org/data/2.5';
+    const appId = 'b31f986179635820b5d31e28a4cf9fc2';
+    // const cityName = 'Kathmandu';
 
-  const cityNameResponse = await axios.get(
-    `${baseURL}/forecast?q=${cityName}&units=metric&appid=${appId}`,
-  );
-  const lat = cityNameResponse?.data?.city?.coord?.lat;
-  const long = cityNameResponse?.data?.city?.coord?.lon;
+    const cityNameResponse = await axios.get(
+      `${baseURL}/forecast?q=${cityName}&units=metric&appid=${appId}`,
+    );
+    const lat = cityNameResponse?.data?.city?.coord?.lat;
+    const long = cityNameResponse?.data?.city?.coord?.lon;
 
-  const latLongResponse = await axios.get(
-    `${baseURL}/onecall?lat=${lat}&lon=${long}&units=metric&appid=${appId}`,
-  );
+    const latLongResponse = await axios.get(
+      `${baseURL}/onecall?lat=${lat}&lon=${long}&units=metric&appid=${appId}`,
+    );
 
-  return {
-    cityName: cityNameResponse?.data?.city?.name,
-    data: latLongResponse?.data,
-  };
-});
+    return {
+      cityName: cityNameResponse?.data?.city?.name,
+      data: latLongResponse?.data,
+    };
+  },
+);
 
 export const weatherSlice = createSlice({
   name: 'weather',
   initialState,
   reducers: {
-    // setCurrentWeather: (state, action) => {
-    //   return {currentWeather: action.payload};
-    // },
+    setCityWeather: (state, action) => {
+      return {cityWeather: action.payload};
+    },
   },
   extraReducers: {
     [getWeather.fulfilled]: (state, action) => {
@@ -39,6 +42,6 @@ export const weatherSlice = createSlice({
   },
 });
 
-export const {setCurrentWeather} = weatherSlice.actions;
+export const {setCityWeather} = weatherSlice.actions;
 
 export default weatherSlice.reducer;
