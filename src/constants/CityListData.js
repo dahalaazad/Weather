3b5c148a3e2d39c83406417afcb1cdb8c6eea5ad
aazad.dahal <aazad.dahal@gurzu.net>
2023-01-05 12:02:38 +0545

    feat: Extract CityList array to src/constants

---
+++ src/constants/CityListData.js
@@ -0,0 +1,8 @@
+import {Images} from './Images';
+
+export const CityListData = [
+  {city: 'Kathmandu', background: Images.sunriseCardBackground},
+  {city: 'London', background: Images.rainyCardBackground},
+  {city: 'Mumbai', background: Images.sunriseCardBackground},
+  {city: 'Doha', background: Images.rainyCardBackground},
+];
