5f4cde4a0410da8a6066cacd54f0eef88793133d
aazad.dahal <aazad.dahal@gurzu.net>
2023-01-05 12:02:56 +0545

    feat: Index for CitylIst array

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
+import {CityListData} from './CityListData';
 import {Colors} from './Colors';
 import {Images} from './Images';
 import {capitalizeFirstLetterInWords} from './Utils';
 
-export {Colors, Images, capitalizeFirstLetterInWords};
+export {Colors, Images, CityListData, capitalizeFirstLetterInWords};
