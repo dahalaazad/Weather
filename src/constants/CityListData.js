cf322a4c381d56e8ab5c4dc3c3b351f633d4c34e
aazad.dahal <aazad.dahal@gurzu.net>
2023-01-10 14:20:56 +0545

    fix: Remove unused static data

--- src/constants/CityListData.js
+++
@@ -1,8 +0,0 @@
-import {Images} from './Images';
-
-export const CityListData = [
-  {city: 'Kathmandu', background: '1'},
-  {city: 'London', background: '2'},
-  {city: 'Mumbai', background: '1'},
-  {city: 'Doha', background: '2'},
-];
