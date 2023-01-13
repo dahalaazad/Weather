7759d1d576670b299b81930c469e5cfbfa31a654
aazad.dahal <aazad.dahal@gurzu.net>
2023-01-13 11:32:34 +0545

    fix: Change cityCard bg-image

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -4,4 +4,11 @@ export const Images = {
   sunriseCardBackground: require('@app/assets/images/Sunrise.png'),
   searchDetailBackground: require('@app/assets/images/moon-detail.jpg'),
   sunnyDayBackground: require('@app/assets/images/sunny-sky.jpg'),
+  glassCardBackground: require('@app/assets/images/glass-image.jpg'),
+  woodCardBackground: require('@app/assets/images/wood.jpg'),
+  weatherCardBackground: require('@app/assets/images/weather-bg.png'),
+  weatherCardBackground2: require('@app/assets/images/weather-bg-2.png'),
+  finalCardBackground: require('@app/assets/images/card-bg.webp'),
+  genericCardBackground: require('@app/assets/images/generic-bg.webp'),
+  minimalCardBackground: require('@app/assets/images/minimal.jpg'),
 };
--- src/screens/SearchDetails/components/CityCard.js
+++ src/screens/SearchDetails/components/CityCard.js
@@ -7,7 +7,7 @@ import {
   FlatList,
 } from 'react-native';
 import React from 'react';
-import {Colors} from '@app/constants';
+import {Colors, Images} from '@app/constants';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -15,6 +15,7 @@ import {
 import uuid from 'react-uuid';
 import {getImage} from '@app/utils/getImage';
 import {heightToDp, widthToDp} from '@app/utils';
+import LinearGradient from 'react-native-linear-gradient';
 
 const CityCard = ({
   cityName,
@@ -37,12 +38,10 @@ const CityCard = ({
         onPress={() => onPressAction(item?.city)}
         onLongPress={() => deleteCityCard(item?.city)}>
         <ImageBackground
-          source={getImage(item?.background)}
           style={styles.backgroundContainer}
+          source={Images.minimalCardBackground}
           imageStyle={{borderRadius: 20}}>
-          <Text style={[styles.textStyle, {textAlign: 'center'}]}>
-            {item?.city}
-          </Text>
+          <Text style={[styles.textStyle]}>{item?.city}</Text>
         </ImageBackground>
       </TouchableOpacity>
     </View>
@@ -64,13 +63,15 @@ const CityCard = ({
 export default CityCard;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    // marginHorizontal: 20,
+  },
   cityCardBorderContainer: {
-    height: heightToDp(180),
+    height: heightToDp(175),
     width: widthToDp(140),
     justifyContent: 'center',
-    alignItems: 'center',
-    marginHorizontal: widthToDp(20),
+    alignItems: 'flex-start',
+    marginRight: widthToDp(20),
   },
   cityCardContainer: {
     alignItems: 'flex-start',
@@ -80,22 +81,24 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     height: heightToDp(171),
     width: widthToDp(140),
-
-    justifyContent: 'space-evenly',
-    paddingTop: heightToDp(20),
-    borderRadius: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: heightToDp(20),
+    borderRadius: widthToDp(20),
   },
   textStyle: {
     fontSize: widthToDp(24),
     color: Colors.whiteColor,
     fontWeight: '600',
+    textAlign: 'center',
+    paddingTop: 20,
   },
   selectedListItem: {
     flex: 1,
     // justifyContent: 'center',
     // alignItems: 'center',
-    borderWidth: widthToDp(5),
-    borderColor: 'rgba(0,0,0,0.4)',
+    borderWidth: widthToDp(2),
+    borderColor: 'rgba(0,0,0,0.7)',
     borderRadius: widthToDp(22),
   },
 });
